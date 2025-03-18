e236cafe11a9531d6b26256fb3bd9ba4546d4f53
NamanAgarwal08 <naman.agarwal_cs21@gla.ac.in>
2025-03-18 09:12:59 +0530

    Add. RabbitMW

---
+++ src/main/java/com/example/Address/Book/interfaces/IAuthInterface.java
@@ -0,0 +1,26 @@
+package com.example.Address.Book.interfaces;
+
+import com.example.Address.Book.dto.AuthUserDTO;
+import com.example.Address.Book.dto.LoginDTO;
+import com.example.Address.Book.dto.PassDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IAuthInterface {
+
+    public String register (AuthUserDTO user);
+
+
+    public String login(LoginDTO user, HttpServletResponse response);
+
+    public AuthUserDTO forgotPassword(PassDTO pass, String email);
+
+    public String resetPassword(String email, String currentPass, String newPass);
+
+    public String logout(HttpServletRequest request, HttpServletResponse response);
+
+    public String clear();
+
+}
